2ad0abb1c43b74f52491eb9d654544ebd743ae56
PeilunLi <lpl6338236@gmail.com>
2020-07-14 20:35:20 +0800

    Add dependency.

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 # export FLASK_APP=http_server.py
 # flask run
-pip3 install sqlitedict flask jsonrpcclient eth_utils
+pip3 install sqlitedict flask jsonrpcclient eth_utils gunicorn
 gunicorn -w 1 -b 127.0.0.1:4000 http_server:app

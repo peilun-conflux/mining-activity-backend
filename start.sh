e1ee086fcafb09aed67ef3d2e9eb0ea740388028
PeilunLi <lpl6338236@gmail.com>
2020-07-16 16:01:05 +0800

    nit.

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 # flask run
 
 # sudo apt install -y python3.7 python3.7-dev
-pip3 install sqlitedict flask jsonrpcclient eth_utils gunicorn asyncio websockets pysha3 py_ecc rlp coincurve
+# pip3 install sqlitedict flask jsonrpcclient eth_utils gunicorn asyncio websockets pysha3 py_ecc rlp coincurve
 gunicorn -w 1 -b 127.0.0.1:4000 http_server:app

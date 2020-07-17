d3cc341781cbd37411aa8686e19ff1dc0b98cca0
PeilunLi <lpl6338236@gmail.com>
2020-07-17 16:42:03 +0800

    Update listen IP.

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 
 # sudo apt install -y python3.7 python3.7-dev
 # pip3 install sqlitedict flask jsonrpcclient eth_utils gunicorn asyncio websockets pysha3 py_ecc rlp coincurve
-gunicorn -w 1 -b 127.0.0.1:4000 http_server:app
+gunicorn -w 1 -b 0.0.0.0:4000 http_server:app

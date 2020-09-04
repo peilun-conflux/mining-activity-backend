20668c086b9246794f26eec870d3915ccd75d676
PeilunLi <lpl6338236@gmail.com>
2020-09-04 16:33:23 +0800

    foreground.

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@
 # pip3 install sqlitedict flask jsonrpcclient eth_utils gunicorn asyncio websockets pysha3 py_ecc rlp coincurve flask_cors gevent psutil uwsgi
 # gunicorn -b 0.0.0.0:4000 http_server:app
 # uwsgi --http 0.0.0.0:4000 --module http_server:app
-python3 chain_data_fetcher.py &
+python3 chain_data_fetcher.py
